bb47ffe49d29403695b96f6e978ac91fb5821c40
Brian Bradley <bbradle2@gmail.com>
2024-03-04 11:38:54 -0500

    test

--- Program.cs
+++ Program.cs
@@ -1,61 +1,64 @@
 ﻿namespace first_test;
 
-using System.Data.Common;
+//using System.Data.Common;
 using System.Device.Gpio;
-using MySqlConnector;
-using Dapper;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Net.NetworkInformation;
+//using MySqlConnector;
+//using Dapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using System.Text.Json.Serialization;
-using System.Text.Json;
-using System.Globalization;
 using System.Text;
 using DataObjects;
 using LinuxExtensions;
-
-
-
+using System;
 
 class Program
 {
-    
-    private static readonly GpioController controller = new GpioController();
-    private static readonly int pin = 23;
-    private static void SendMessageToTerminal(string message) 
-    {
-        Console.WriteLine(message);
-    }
-    private static void OnSigInt(object? sender, ConsoleCancelEventArgs e)
-    {
-        controller.Write(pin, PinValue.Low);
-        controller.ClosePin(pin);
-        SendMessageToTerminal("SIGINT Received...");
-    }
 
-    private static void OnSigTerm(object? sender, EventArgs e)
-    {
-        controller.Write(pin, PinValue.Low);
-        controller.ClosePin(pin);
-        SendMessageToTerminal("SIGTERM Received...");
-    }
+    //private static readonly GpioController controller = new GpioController();
+    private static readonly int pin = 23;
+    // private static void SendMessageToTerminal(string message) 
+    // {
+    //     Console.WriteLine(message);
+    // }
+
+    //  private static void OnSigInt(object? sender, ConsoleCancelEventArgs e)
+    //  {
+    //     gpController.Write(pin, PinValue.Low);
+    //     gpController.ClosePin(pin);
+    //      SendMessageToTerminal("SIGINT Received...");
+    //  }
+
+    // private static void OnSigTerm(object? sender, EventArgs e)
+    // {
+    //     gpController.Write(pin, PinValue.Low);
+    //     gpController.ClosePin(pin);
+    //     SendMessageToTerminal("SIGTERM Received...");
+    // }
 
     static void Main(string[] args)
     {
 
-        AppDomain.CurrentDomain.ProcessExit += OnSigTerm;
-        Console.CancelKeyPress += OnSigInt;
+        // AppDomain.CurrentDomain.ProcessExit += OnSigTerm;
+        // Console.CancelKeyPress += OnSigInt;
 
         var builder = WebApplication.CreateBuilder(args);
-
+        builder.Services.AddSingleton<GpioController>();
+        
         var app = builder.Build();
+        app.Use(async (context, next) =>
+        {
+            // Do work that can write to the Response.
+            await next.Invoke();
+            // Do logging or other work that doesn't write to the Response.
+        });
+        //var config = app.Configuration;
+        //var s = app.Services.GetService<GpioController>();
 
-        app.MapGet("/cpuinfo", async () =>
+        app.MapGet("/cpuinfo", async (HttpContext context) =>
         {
             StringBuilder? retRes = new StringBuilder();
+            
             try
             {
                 var commandRes = await Task.Run(() => "cat /proc/cpuinfo".Execute());
@@ -69,65 +72,68 @@ class Program
 
                 CPUObject cpuObject = new CPUObject { Call = "CPUInfo", Content = retRes.ToString() };
 
-                return Results.Json(cpuObject);
-            } 
-            finally 
+                return Results.Ok(cpuObject);
+            }
+            finally
             {
                 retRes = null;
             }
         });
 
-        app.MapGet("/ledstatus", () =>
+        app.MapGet("/ledstatus", (GpioController gpioController) =>
         {
-            var openPin = controller.OpenPin(pin, PinMode.Output);
+            ArgumentNullException.ThrowIfNull(gpioController);
+
+            var openPin = gpioController.OpenPin(pin, PinMode.Output);
 
             try
-            {        
+            {
                 return Results.Json(openPin.Read().ToString());
-            } 
-            finally 
+            }
+            finally
             {
-                
+
             }
         });
 
-        app.MapGet("/ledon", () =>
+        app.MapGet("/ledon", (GpioController gpioController) =>
         {
-            var openPin = controller.OpenPin(pin, PinMode.Output);
+            ArgumentNullException.ThrowIfNull(gpioController);
+
+            var openPin = gpioController.OpenPin(pin, PinMode.Output);
 
             try
             {
-                controller.Write(pin, PinValue.High);
+                gpioController.Write(pin, PinValue.High);
                 var pinValue = openPin.Read();
                 return Results.Json(pinValue.ToString());
             }
             finally
             {
-                
+
             }
         });
 
-        app.MapGet("/ledoff", () =>
+        app.MapGet("/ledoff", (HttpContext context, GpioController gpioController) =>
         {
-            var openPin = controller.OpenPin(pin, PinMode.Output);
+            
+            ArgumentNullException.ThrowIfNull(gpioController);
+
+            var openPin = gpioController.OpenPin(pin, PinMode.Output);
 
             try
             {
-                controller.Write(pin, PinValue.Low);
+                gpioController.Write(pin, PinValue.Low);
                 var pinValue = openPin.Read();
                 return Results.Json(pinValue.ToString());
             }
             finally
             {
-                
+
             }
         });
 
         app.Run();
-        controller.OpenPin(pin, PinMode.Output);
-        controller.Write(pin, PinValue.Low);
-        controller.ClosePin(pin);
-        AppDomain.CurrentDomain.ProcessExit -= OnSigTerm;
-        Console.CancelKeyPress -= OnSigInt;
+
     }
 }
