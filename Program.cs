f06c03d892e67e92d44db664c776ae95e84ed6f9
Brian Bradley <bbradle2@gmail.com>
2024-03-04 18:52:12 -0500

    Removed un-nessescary code

--- Program.cs
+++ Program.cs
@@ -1,67 +1,31 @@
-﻿namespace first_test;
-
-//using System.Data.Common;
-using System.Device.Gpio;
-//using MySqlConnector;
-//using Dapper;
+﻿using System.Device.Gpio;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using Interfaces;
-using Controllers;
 
-class Program
+
+namespace first_test
 {
+    using Interfaces;
+    using Controllers;
 
-    //private static readonly GpioController controller = new GpioController();
-    
-    // private static void SendMessageToTerminal(string message) 
-    // {
-    //     Console.WriteLine(message);
-    // }
-
-    //  private static void OnSigInt(object? sender, ConsoleCancelEventArgs e)
-    //  {
-    //     gpController.Write(pin, PinValue.Low);
-    //     gpController.ClosePin(pin);
-    //      SendMessageToTerminal("SIGINT Received...");
-    //  }
-
-    // private static void OnSigTerm(object? sender, EventArgs e)
-    // {
-    //     gpController.Write(pin, PinValue.Low);
-    //     gpController.ClosePin(pin);
-    //     SendMessageToTerminal("SIGTERM Received...");
-    // }
-
-    static void Main(string[] args)
+    class Program
     {
-
-        // AppDomain.CurrentDomain.ProcessExit += OnSigTerm;
-        // Console.CancelKeyPress += OnSigInt;
-
-        var builder = WebApplication.CreateBuilder(args);
-        builder.Services.AddSingleton<GpioController>();
-        builder.Services.AddSingleton<IRaspberryPiController, RaspberryPiController>();
-
-        var app = builder.Build();
-        var piController = app.Services.GetService<IRaspberryPiController>();
-        piController!.Start(app);
-
-        app.Use(async (context, next) =>
+       
+        static void Main(string[] args)
         {
-            try
-            {
-                // Do work that can write to the Response.
-                await next.Invoke();
-                // Do logging or other work that doesn't write to the Response.
-            } 
-            catch (Exception e) 
-            {
-                Console.WriteLine(e.ToString());
-            }
-        });
-      
-        app.Run();
 
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddSingleton<GpioController>();
+            builder.Services.AddSingleton<IRaspberryPiController, RaspberryPiController>();
+
+            var app = builder.Build();
+
+            var piController = app.Services.GetService<IRaspberryPiController>();
+            ArgumentNullException.ThrowIfNull(piController);
+            piController.Start(app);
+            
+            app.Run();
+
+        }
     }
 }
