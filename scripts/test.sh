d7d4a5b48ee54dde449456a35a2fe9581ea2e567
Brian Bradley <bbradle2@gmail.com>
2024-03-15 13:27:14 -0400

    testing script changes

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,12 +1,19 @@
 printf "Start Test\n"
+port=5000
 
-printf "Calling http://localhost:5000/RaspberryPiInfo/GetMemoryInfo\n"
-curl -s -X "GET" "http://localhost:5000/RaspberryPiInfo/GetMemoryInfo" -H "accept: plain/text" | jq > Memoryinfo.json
+printf "Calling http://localhost:$port/RaspberryPiInfo/GetMemoryInfo\n"
+memoryinfocount=$(curl -X GET http://localhost:$port/RaspberryPiInfo/GetMemoryInfo | jq | wc -l) # Memoryinfo.json
 
-printf "Calling http://localhost:5000/RaspberryPiInfo/GetCPUInfo\n"
-curl -s -X "GET" "http://localhost:5000/RaspberryPiInfo/GetCPUInfo" -H "accept: plain/text" | jq > CPUInfo.json
+if [ $memoryinfocount -gt 0 ]; 
+then
+    printf 'Success.\n'
+else
+    printf 'Fail.\n'
+fi
+#printf "Calling http://localhost:5000/RaspberryPiInfo/GetCPUInfo\n"
+#curl -X GET http://localhost:5000/RaspberryPiInfo/GetCPUInfo | jq > CPUInfo.json
 
-printf "Calling http://localhost:5000/RaspberryPiInfo/GetSystemInfo\n"
-curl -s -X "GET" "http://localhost:5000/RaspberryPiInfo/GetSystemInfo" -H "accept: plain/text" | jq > SystemInfo.json
+#printf "Calling http://localhost:5000/RaspberryPiInfo/GetSystemInfo\n"
+#curl -X GET http://localhost:5000/RaspberryPiInfo/GetSystemInfo | jq > SystemInfo.json
 
 printf "End Test\n"
