7f42fc3c1df7442fd071b37729e8cb4b46b5bd9d
Brian Bradley <bbradle2@gmail.com>
2024-03-29 18:25:40 -0400

    fixed test.sh to exit properly

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,11 +1,17 @@
+#!/bin/bash
 #assign global variables
 port=5000
 programuser=$USER
 host="localhost"
-
+process_id=$!
 
 #start test
 printf "Start Test\n"
+cd ..
+dotnet run &
+printf "Waiting for server to build and start\n\n"
+sleep 5
+
 
 printf "###########\n\n"
 
@@ -39,6 +45,12 @@ runtest "http://$host:$port/RaspberryPiInfo/GetMemoryInfo"
 runtest "http://$host:$port/RaspberryPiInfo/GetCPUInfo"
 runtest "http://$host:$port/RaspberryPiInfo/GetSystemInfo"
 
+#for pid in $(jobs -p); do kill $pid; done
+
 printf "###########\n"
 printf "End Test\n"
+kill $(jobs -p)
+
+wait $process_id
+#echo "Exit status: $?"
 #end test
